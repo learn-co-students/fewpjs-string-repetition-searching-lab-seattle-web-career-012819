3640bbe82b1e5164437de4e79855308acc08d090
elimerrell <elijah.merrell@gmail.com>
2019-03-12 16:56:02 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+const verse = "Na na na na na na na, na na na na, hey Jude."
+
+function heyJude(verse){
+    return verse.repeat(16)
+}
+
+function prohibitedLanguage(string) {
+ let myRegExp = /candycorn|brusselssprouts/;
+ let searchResult = string.search(myRegExp);
+ return searchResult;
+}
