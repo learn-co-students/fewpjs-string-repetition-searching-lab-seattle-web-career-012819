de132dca7f7400dd0213a318ed72e4234ffddf65
Lucy Suddenly <lucysuddenly@Lucys-MacBook-Air.local>
2019-03-12 17:39:37 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution here
+
+function heyJude(){
+  return "Na na na na na na na, na na na na, hey Jude.".repeat(16)
+}
+
+function prohibitedLanguage(string){
+  let myRegExp = /candycorn|brusselssprouts/;
+  return string.search(myRegExp)
+}
