f70fe51cb75e76a2c9dcc09a97e7175fb5fd44b8
tiefengeist <sahirnambiar@gmail.com>
2019-03-13 17:27:14 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Code your solution here
+function heyJude() {return "Na na na na na na na, na na na na, hey Jude.".repeat(16)}
+
+function prohibitedLanguage(str){
+let prohibited = /candycorn|brusselssprouts/;
+return str.search(prohibited);
+}
