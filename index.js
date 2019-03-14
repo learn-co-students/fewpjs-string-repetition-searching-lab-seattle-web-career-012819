b247493a3b9ff4b4242d1d4322c78013185efdb5
Madison Stankevich <madison.stankevich@flatironschool.com>
2019-03-13 21:27:34 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution here
+function heyJude(lyric){
+  return lyric.repeat(16)
+}
+
+function prohibitedLanguage(input){
+  let banned = /candycorn|brusselssprouts/;
+    return input.search(banned);
+}
