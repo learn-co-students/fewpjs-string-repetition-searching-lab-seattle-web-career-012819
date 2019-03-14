03b787cc55dfc9bc43d21637ade4507bdc5ebed0
allen-woods <allen.woods@flatironschool.com>
2019-03-13 23:01:32 -0700

    First commit.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solution here
+function heyJude(str, num=1) {
+  return str.repeat(num);
+};
+
+function prohibitedLanguage(str) {
+  let bannedUsers = /candycorn|brusselssprouts/;
+  return str.search(bannedUsers);
+}
