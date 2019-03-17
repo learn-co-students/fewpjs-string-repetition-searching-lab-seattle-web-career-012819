dd8607c302b80936e85011923fcdd743526d7d5f
halented <hallie.dunn1@gmail.com>
2019-03-17 16:21:27 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Code your solution here
+import { stringify } from "querystring";
+
+function heyJude() {
+    return "Na na na na na na na, na na na na, hey Jude.".repeat(16)
+}
+
+function prohibitedLanguage(username) {
+    let tada = /candycorn|brusselssprouts/
+    return username.search(tada)
+}
