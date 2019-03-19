5ae867f405aa70c73647946630c1d335ed3de640
Nat-Reid <nathanielreid@gmail.com>
2019-03-18 17:19:27 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function repeat(string, num){
+  return (new Array(num+1)).join(string);
+}
+
+function heyJude(){
+  return repeat("Na na na na na na na, na na na na, hey Jude.", 16);
+}
+
+function prohibitedLanguage(string){
+  return string.search(/candycorn|brusselssprouts/);
+}
